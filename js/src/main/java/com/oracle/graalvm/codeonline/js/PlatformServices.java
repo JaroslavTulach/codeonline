92bb8b8c415a62550feb6ef1d8852ebf6638e181
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2021-06-10 08:40:15 +0200

    Using Exports.eval instead of @JavaScriptBody

--- js/src/main/java/com/oracle/graalvm/codeonline/js/PlatformServices.java
+++ js/src/main/java/com/oracle/graalvm/codeonline/js/PlatformServices.java
@@ -1,6 +1,5 @@
 package com.oracle.graalvm.codeonline.js;
 
-import net.java.html.js.JavaScriptBody;
 import net.java.html.lib.Modules;
 import net.java.html.lib.Objs;
 
@@ -10,12 +9,9 @@ public class PlatformServices {
             @Override
             protected Objs find(String string) {
                 if(string.equals("CodeMirror"))
-                    return net.java.html.lib.codemirror.Exports.$as(getCodeMirror());
+                    return net.java.html.lib.codemirror.Exports.$as(net.java.html.lib.Exports.eval("CodeMirror"));
                 return null;
             }
         };
     }
-
-    @JavaScriptBody(args = {}, body = "return CodeMirror;")
-    private static native Object getCodeMirror();
 }
